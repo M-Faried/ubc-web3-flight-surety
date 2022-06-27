d5cf08fd0e51254ae610e6443153fce881c5b701
M Faried <m.a.faried@gmail.com>
2022-06-27 10:26:28 +0200

    Updated the configuration to connect to the ganache UI for testing.

--- truffle-config.js
+++ truffle-config.js
@@ -1,19 +1,31 @@
+const { INFURA_NETWORK_ID, WALLET_MNEOMONIC } = require('./secrets.js');
 var HDWalletProvider = require("truffle-hdwallet-provider");
-var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
 
 module.exports = {
   networks: {
     development: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
-      },
-      network_id: '*',
-      gas: 9999999
-    }
+      // provider: function () {
+      //   return new HDWalletProvider(mnemonic, "http://127.0.0.1:7545/", 0, 30);
+      // },
+      host: "localhost",
+      port: 7545,
+      network_id: '5777',
+      // gas: 4500000,
+      // gasPrice: 10000000000
+      // host: "localhost",
+      // port: 9545,
+      // network_id: "5777"
+    },
+    ropsten: {
+      provider: () => new HDWalletProvider(WALLET_MNEOMONIC, `https://ropsten.infura.io/v3/${INFURA_NETWORK_ID}`),
+      network_id: 3,       // rinkeby's id
+      gas: 4500000,        // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000
+    },
   },
   compilers: {
     solc: {
-      version: "^0.4.24"
+      version: "^0.4.26"
     }
   }
 };
