db66ffe2d002487e61ea57abe05b70acb098f7d4
M Faried <m.a.faried@gmail.com>
2022-06-27 10:22:53 +0200

    Updated the test configuration after instructing ganache to create 30 accounts.

--- config/testConfig.js
+++ config/testConfig.js
@@ -4,6 +4,8 @@ var FlightSuretyData = artifacts.require("FlightSuretyData");
 var BigNumber = require('bignumber.js');
 
 var Config = async function (accounts) {
+    // console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>Test Config Accounts');
+    // console.log(accounts);
 
     // These test addresses are useful when you need to add
     // multiple users in test scripts
@@ -19,22 +21,23 @@ var Config = async function (accounts) {
     //     "0x2f2899d6d35b1a48a4fbdc93a37a72f264a9fca7"
     // ];
 
-    let testAddresses = [
-        "0x7dbeef232a64ec5a96b65f2cc32c7aceeb31e80c",
-        "0x157f57e4a6d1886af4065859319fba08a6340278",
-        "0xbacbce688a6ad9f2203819a18f8f6a5d6ad7c946",
-        "0xdb783458c2eb0dee8dd0792e3245e6e159846186",
-        "0xfc32f1b6653913c9d0a5a2afba65fcd8d0781121",
-        "0x2b5a206fe6d619d07c8749dffbe068f8fc12061a",
-        "0xfe4bf2a585b1f8a97d869fc384ca9589ea965905",
-        "0xd634117f9ab82b02665edebc65a055d4dfb00b95",
-        "0x45aec21c996ab4309367461d648c8d3fb4e2b69c",
-        "0x942358d61b5c8b0922e25ada47f351cc60ef218d"
-    ];
+    // let testAddresses = [
+    //     "0x9B3f891A30b60C05bAb8F7F98A38C6B1E6885B76",
+    //     "0x20Ec98eF5E7992dD5EfdbA8526f851b3Dd796422",
+    //     "0xf20E7E2e194d30085e43631a376bbB1C40B19770",
+    //     "0x4f8494F7Eb343cd5bDA68150E596F171768946E6",
+    //     "0xAA6bE1Fb9c65111E7365440e9149804cc85F554f",
+    //     "0xd4F5f91D1f07538e420cbF1b58a18B1e6b9aA91B",
+    //     "0x861a9781FD844Df2eF7C6942c8d08DCe86E1F4e8",
+    //     "0x2e1d3F0B173F36BB731b33621086AE805af6Db9f",
+    //     "0xB426d52FF6cE764FAb31004D490FA81A857f1dcB",
+    //     "0x95DfEA08e8ad507e0Df3E57926abed765f07CF0B",
+    // ];
 
 
     let owner = accounts[0];
     let firstAirline = accounts[1];
+    let testAddresses = accounts.slice(2);
 
     let flightSuretyData = await FlightSuretyData.new(firstAirline, { from: owner });
     let flightSuretyApp = await FlightSuretyApp.new(flightSuretyData.address, { from: owner });
