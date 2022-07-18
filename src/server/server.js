b7dcd2714f95ff74faa16742fa7dc395b6099c04
M Faried <m.a.faried@gmail.com>
2022-07-18 12:41:05 +0200

    Increased the number of oracles to 3 which is the requried minimum number of oracles.

--- src/server/server.js
+++ src/server/server.js
@@ -5,27 +5,36 @@ import express from 'express';
 import { Random } from "random-js";
 
 
+const ORACLES_COUNT = 3;
 let config = Config['localhost'];
 let web3 = new Web3(new Web3.providers.WebsocketProvider(config.url.replace('http', 'ws')));
 web3.eth.defaultAccount = web3.eth.accounts[0];
 
 
 let _flightSuretyApp = new web3.eth.Contract(FlightSuretyApp.abi, config.appAddress);
-let _oracle = {};
+let _oracles = [];
 
-const registerOracle = async () => {
+const registerOracles = async () => {
   try {
     // Retrieving all the accounts.
     const accounts = await web3.eth.getAccounts();
-    // Dedicating the last account of ganache to the oracle.
-    const oracleAccount = accounts[accounts.length - 1];
     const registrationFees = web3.utils.toWei("1", "ether");
-    await _flightSuretyApp.registerOracle({ from: oracleAccount, value: registrationFees });
-    let index = await _flightSuretyApp.getMyIndexes({ from: oracleAccount });
-    _oracle = {
-      index,
-      account: oracleAccount
-    };
+
+    // Dedicating the last 3 accounts of ganache to the oracle.
+    const oracleAccounts = accounts.slice(accounts.length - ORACLES_COUNT);
+
+    for (let i = 0; i < ORACLES_COUNT; i++) {
+      // Registering the oracle.
+      await _flightSuretyApp.registerOracle({ from: oracleAccounts[i], value: registrationFees });
+      // Fetching the index of the oracle.
+      let index = await _flightSuretyApp.getMyIndexes({ from: oracleAccounts[i] });
+      // Adding the oracle account to the list of the oracles.
+      _oracles.push({
+        index,
+        account: oracleAccounts[i]
+      });
+    }
+
     console.log('The oracle was registred successfully.');
   }
   catch (e) {
@@ -33,7 +42,7 @@ const registerOracle = async () => {
   }
 }
 
-registerOracle();
+registerOracles();
 
 _flightSuretyApp.events.OracleRequest({
   fromBlock: 0
@@ -50,18 +59,20 @@ _flightSuretyApp.events.OracleRequest({
 
     // console.log(`Oracle Received Request: ID ${requestFlight.flight} Airline ${requestFlight.airline} sent status ${statusCode}`);    
 
-    _flightSuretyApp.submitOracleResponse(
-      _oracle.index,
-      requestFlight.airline,
-      requestFlight.flight,
-      requestFlight.timestamp,
-      statusCode,
-      { from: _oracle.account }
-    ).then(res => {
-      console.log(`Flight status was submitted successfully. STATUS: ${statusCode}`);
-    }).catch(err => {
-      console.log("Failed to submit oracle request.", err);
-    });
+    for (let i = 0; i < ORACLES_COUNT; i++) {
+      _flightSuretyApp.submitOracleResponse(
+        _oracles[i].index,
+        requestFlight.airline,
+        requestFlight.flight,
+        requestFlight.timestamp,
+        statusCode,
+        { from: _oracles[i].account }
+      ).then(res => {
+        console.log(`Flight status was submitted successfully. STATUS: ${statusCode} INDEX: ${_oracles[i].index}`);
+      }).catch(err => {
+        console.log(`Failed to submit oracle request. INDEX: ${_oracles[i].index}`, err);
+      });
+    }
 
   }
 });
